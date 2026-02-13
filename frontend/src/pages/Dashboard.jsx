5bde894ad46d2f09fbe731a73a1f22cc28ff5572
juhi55 <cjuhi888@gmail.com>
2026-02-13 12:42:46 +0530

    imported requestform to dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,12 @@
+import RequestForm from "../components/RequestForm";
+
 function Dashboard() {
   return (
     <div style={{ padding: "20px" }}>
       <h2>Dashboard</h2>
       <p>Welcome to the maintenance system.</p>
+
+      <RequestForm />
     </div>
   );
 }
